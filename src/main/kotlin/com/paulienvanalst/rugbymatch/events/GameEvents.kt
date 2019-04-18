838d6649666cc97c1f8a481ade6226794e4cef66
Abderrahmen KALLEL <abderrahmen.kallel-extern@renault.com>
2019-04-18 13:56:39 +0200

    implement ex2

--- src/main/kotlin/com/paulienvanalst/rugbymatch/events/GameEvents.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/events/GameEvents.kt
@@ -1,9 +1,23 @@
 package com.paulienvanalst.rugbymatch.events
 
+import com.paulienvanalst.rugbymatch.game.LineOut
+import com.paulienvanalst.rugbymatch.game.Scrum
 import com.paulienvanalst.rugbymatch.game.SetPiece
 import com.paulienvanalst.rugbymatch.team.TeamName
 import org.springframework.context.ApplicationEvent
 
-sealed class SetPieceEvent(source: Any, val setPiece: SetPiece, val winningTeam: TeamName) : ApplicationEvent(source)
+sealed class SetPieceEvent(source: Any, val setPiece: SetPiece, val winningTeam: TeamName) : ApplicationEvent(source) {
 
 
+}
+
+class ScrumWasPlayed(source: Any, scrum: Scrum, winningTeam: TeamName) : SetPieceEvent(source, scrum, winningTeam) {
+
+}
+
+class LineOutWasPlayed(source: Any, line: LineOut, winningTeam: TeamName) : SetPieceEvent(source, line, winningTeam)
+
+fun List<SetPieceEvent>.wonBy(team: TeamName) = this.filter { it.winningTeam == team }
+fun List<SetPieceEvent>.lostBy(team: TeamName) = this.filter { it.winningTeam != team }
+fun List<SetPieceEvent>.scrumEvents() =  this.filter { it.setPiece is Scrum }
+fun List<SetPieceEvent>.lineOutEvents() = this.filter { it.setPiece is LineOut }
